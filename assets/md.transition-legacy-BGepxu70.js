dcb9408881395f60c9c5bc212234cccddddf820a
Beazel17 <ayunosabanpan@gmail.com>
2025-04-04 15:05:40 -1200

    Updates

--- assets/md.transition-legacy-BGepxu70.js
+++ assets/md.transition-legacy-BGepxu70.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Cz5VmUDm.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-BVrx1_qI.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
