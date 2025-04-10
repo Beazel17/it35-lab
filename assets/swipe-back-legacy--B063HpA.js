6268d5f913e898d6aadbb32a8f51a7549b0b7138
beazel17 <ayunosabanpan@gmail.com>
2025-04-10 14:15:46 +0800

    Updates

--- assets/swipe-back-legacy--B063HpA.js
+++ assets/swipe-back-legacy--B063HpA.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-sN2wBqpy.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-v6_pkvQG.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
